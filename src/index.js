3956abdf200d63e6f19a534a43a93b5700720e77
Yuri Vaz <yurigava@gmail.com>
2019-12-07 01:53:02 -0300

    Authorization Working

---
+++ src/index.js
@@ -0,0 +1,56 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+var express = require('express');
+var order = require('./playlistOrder')
+const args = require('yargs').argv;
+var app = express();
+var PORT=8080;
+var scopes = ['user-read-private', 'user-read-playback-state', 'playlist-modify-public',
+    'playlist-modify-private'],
+  redirectUri = 'http://localhost:8080/callback',
+  clientId = 'b87fd794fa18420d8a73507fd3d93989',
+  clientSecret= args.secret,
+  state = 'some-state-of-my-choice';
+
+var spotifyApi = new SpotifyWebApi({
+  redirectUri: redirectUri,
+  clientId: clientId,
+  clientSecret: clientSecret
+});
+
+app.get('/', (req, res) => {
+      var authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
+      res.redirect(authorizeURL);
+})
+
+app.get('/callback', async (req, res) => {
+    res.send('You are successfully logged in');
+    var data = await spotifyApi.authorizationCodeGrant(req.query.code)
+        .catch((err) => {
+            console.log('Something went wrong!', err);
+        });
+    if(data) {
+        spotifyApi.setAccessToken(data.body['access_token']);
+        spotifyApi.setRefreshToken(data.body['refresh_token']);
+    } else {
+        console.log('Please Login')
+    }
+})
+
+app.get('/current', async (req,res) => {
+    var current = await spotifyApi.getMyCurrentPlaybackState({
+    })
+        .catch((err) => {
+            console.log('Something went wrong!', err);
+        });
+    if(current) {
+        res.send(current.body);
+    } else {
+        res.send('You are not logged.')
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`app listening on port: ${PORT}`)
+})
+
+setInterval(() => { order.orderPlaylist(spotifyApi) }, 2 * 60 * 1000)
---
+++ src/playlistOrder.js
@@ -0,0 +1,8 @@
+var currentSong
+var users = new Map();
+
+const orderPlaylist = (spotifyApi) => {
+
+}
+
+exports.orderPlaylist = orderPlaylist;
