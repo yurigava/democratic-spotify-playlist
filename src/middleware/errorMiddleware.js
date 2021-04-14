0812fe619ca74324a6ed6a348c20f9621bc0fcde
Murilo Kramar <murilogabardo@gmail.com>
2021-04-14 09:17:42 -0300

    Updates error handler to not expose information regarding Spotify Errors

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -2,11 +2,11 @@ const GeneralError = require('../errors/GeneralError')
 const { WebapiError } = require('spotify-web-api-node/src/response-error')
 
 function errorHandler (err, req, res, next) {
-  console.log(err.stack)
   if (err instanceof GeneralError) {
+    console.log(err.stack)
     res.status(err.code).send({ message: err.message })
   } else if (err instanceof WebapiError) {
-    res.status(err.statusCode).send(err.body)
+    res.status(err.statusCode).send({ message: 'An error occured while communicating with Spotify Api' })
   }
 }
 
