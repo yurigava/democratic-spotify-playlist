ac9a73782c618546ce3791f54c6b2ac095cc381c
Vaz, Yuri <yurigava@gmail.com>
2022-02-25 14:01:13 -0300

    Rolling back change in decision to move a track. Needs further investigation

--- src/services/playlistMovementCalculator.js
+++ src/services/playlistMovementCalculator.js
@@ -3,7 +3,7 @@ function getPlaylistReorderMovements (currentPlaylistTracks, toBePlaylistTracks)
   const movingPlaylistTracks = [...currentPlaylistTracks]
   for (const positionInToBePlaylist of toBePlaylistTracks.keys()) {
     const toBeTrackEntry = toBePlaylistTracks[positionInToBePlaylist]
-    if (!areTrackEntriesEqual(movingPlaylistTracks[positionInToBePlaylist], toBeTrackEntry)) {
+    if (areTrackEntriesDifferent(movingPlaylistTracks[positionInToBePlaylist], toBeTrackEntry)) {
       const positionInCurrentPlaylist = movingPlaylistTracks.findIndex(currentTrackEntry => areTrackEntriesEqual(currentTrackEntry, toBeTrackEntry))
       movements.push({ from: positionInCurrentPlaylist, to: positionInToBePlaylist })
       moveTrack(movingPlaylistTracks, positionInCurrentPlaylist, positionInToBePlaylist)
@@ -16,6 +16,11 @@ function moveTrack (playlistTracks, fromPosition, toPosition) {
   const track = playlistTracks[fromPosition]
   playlistTracks.splice(fromPosition, 1)
   playlistTracks.splice(toPosition, 0, track)
+}
+
+function areTrackEntriesDifferent(entry1, entry2) {
+  return entry1.track.id !== entry2.track.id &&
+      entry1.added_by.id !== entry2.added_by.id
 }
 
 function areTrackEntriesEqual (entry1, entry2) {
