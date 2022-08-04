dd7af950c8307a1b3d8e380431ad0d748dd1878e
Vaz, Yuri <yurigava@gmail.com>
2022-08-03 21:33:07 -0300

    Setting reorder to be sequential

--- src/clients/SpotifyClientWrapper.js
+++ src/clients/SpotifyClientWrapper.js
@@ -1,35 +1,45 @@
-const SpotifyWebApi = require('spotify-web-api-node')
+const SpotifyWebApi = require("spotify-web-api-node");
 
-class SpotifyClientWrapper {
-  constructor (credentials) {
-    this.spotifyApi = new SpotifyWebApi(credentials)
+class SpotifyClientWrapper
+{
+  constructor(credentials)
+  {
+    this.spotifyApi = new SpotifyWebApi(credentials);
   }
 
-  async authenticate (code) {
-    const data = await this.spotifyApi.authorizationCodeGrant(code).catch((err) => {
-      console.log('Something went wrong!', err)
-    })
+  async authenticate(code)
+  {
+    const data = await this.spotifyApi
+      .authorizationCodeGrant(code)
+      .catch((err) =>
+      {
+        console.log("Something went wrong!", err);
+      });
 
-    if (data) {
-      console.log(data.body)
-      return data.body
-    } else {
-      console.log('Please Login')
-      return ['', '']
+    if (data)
+    {
+      console.log(data.body);
+      return data.body;
     }
+
+    console.log("Please Login");
+    return ["", ""];
   }
 
-  createAuthorizeURL (scopes, state) {
-    return this.spotifyApi.createAuthorizeURL(scopes, state)
+  createAuthorizeURL(scopes, state)
+  {
+    return this.spotifyApi.createAuthorizeURL(scopes, state);
   }
 
-  async retrievePlaylistTracks (playlistId) {
+  async retrievePlaylistTracks(playlistId)
+  {
     let offsetCounter = 0
     const tracksPerPage = 100
     let tracksInfo = []
     let tracksPage
 
-    do {
+    do
+    {
       tracksPage = await this.spotifyApi
         .getPlaylistTracks(playlistId, {
           offset: offsetCounter,
@@ -37,68 +47,105 @@ class SpotifyClientWrapper {
           fields: 'items(added_at, added_by.id, track(id)), total'
         })
         .then(data => data.body)
-        .catch(err => {
+        .catch(err =>
+        {
           console.error(`Error while retrieving playlist tracks!\nError:${err}`)
         })
       tracksInfo = tracksInfo.concat(tracksPage.items);
       offsetCounter += tracksPerPage;
-    } while(tracksPage.total > offsetCounter)
+    } while (tracksPage.total > offsetCounter)
     return tracksInfo
   }
 
-  retrievePlaylistSnapshotId (playlistId) {
-    return this.spotifyApi.getPlaylist(playlistId, { fields: 'snapshot_id' })
-      .then(data => data.body?.snapshot_id ?? '')
-      .catch(err => {
-        console.error(`Error while retrieving playlist snapshotId!\nError:${err}`)
-        return ''
-      })
+  retrievePlaylistSnapshotId(playlistId)
+  {
+    return this.spotifyApi
+      .getPlaylist(playlistId, {fields: "snapshot_id"})
+      .then((data) => data.body?.snapshot_id ?? "")
+      .catch((err) =>
+      {
+        console.error(
+          `Error while retrieving playlist snapshotId!\nError:${err}`
+        );
+        return "";
+      });
   }
 
-  retrieveCurrentTrackId () {
-    return this.spotifyApi.getMyCurrentPlaybackState()
-      .then(data => data.body.item?.id ?? '')
-      .catch(err => {
-        console.error(`Error while getting User´s playback state!\nError:${err}`)
-        return ''
-      })
+  retrieveCurrentTrackId()
+  {
+    return this.spotifyApi
+      .getMyCurrentPlaybackState()
+      .then((data) => data.body.item?.id ?? "")
+      .catch((err) =>
+      {
+        console.error(
+          `Error while getting User´s playback state!\nError:${err}`
+        );
+        return "";
+      });
   }
 
-  retrieveCurrentUserProfile () {
-    return this.spotifyApi.getMe()
-      .then(data => data.body ?? {})
-      .catch(err => {
-        console.error(`Error while getting User Profile!\nError:${err}`)
-        return ''
-      })
+  retrieveCurrentUserProfile()
+  {
+    return this.spotifyApi
+      .getMe()
+      .then((data) => data.body ?? {})
+      .catch((err) =>
+      {
+        console.error(`Error while getting User Profile!\nError:${err}`);
+        return "";
+      });
   }
 
-  retrieveUserPlaylists () {
-    return this.spotifyApi.getUserPlaylists({ limit: 50 })
-      .then(data => data.body?.items ?? [])
-      .catch(err => {
-        console.error(`Error while retrieving User Playlists!\nError:${err}`)
-        return []
-      })
+  retrieveUserPlaylists()
+  {
+    return this.spotifyApi
+      .getUserPlaylists({limit: 50})
+      .then((data) => data.body?.items ?? [])
+      .catch((err) =>
+      {
+        console.error(`Error while retrieving User Playlists!\nError:${err}`);
+        return [];
+      });
   }
 
-  reorderTracksInPlaylist (playlistId, positionInCurentPlaylist, positionInOrderedPlaylist, options) {
-    console.debug(`Current Position: ${positionInCurentPlaylist}`)
-    console.debug(`New Position: ${positionInOrderedPlaylist}`)
-    return this.spotifyApi.reorderTracksInPlaylist(playlistId, positionInCurentPlaylist, positionInOrderedPlaylist, options)
-      .then(data => data?.body?.snapshot_id ?? '')
-      .catch(err => {
-        console.error(`Error while reordering Tracks in Playlists!\nError:${err}`)
-      })
+  async reorderTracksInPlaylist(
+    playlistId,
+    positionInCurentPlaylist,
+    positionInOrderedPlaylist,
+    options
+  )
+  {
+    console.log(`Moving ${positionInCurentPlaylist} to ${positionInOrderedPlaylist}`);
+    try
+    {
+      const reply = await this.spotifyApi
+        .reorderTracksInPlaylist(
+          playlistId,
+          positionInCurentPlaylist,
+          positionInOrderedPlaylist,
+          options
+        );
+      return reply?.body?.snapshot_id ?? "";
+    } catch (err)
+    {
+      console.error(
+        `Error while reordering Tracks in Playlists!\nError:${err}`
+      );
+      return "";
+    }
   }
 
-  refreshToken () {
-    return this.spotifyApi.refreshAccessToken()
-      .then(data => data?.body ?? {})
-      .catch(err => {
-        console.error(`Error while refreshing token!\nError:${err}`)
-      })
+  refreshToken()
+  {
+    return this.spotifyApi
+      .refreshAccessToken()
+      .then((data) => data?.body ?? {})
+      .catch((err) =>
+      {
+        console.error(`Error while refreshing token!\nError:${err}`);
+      });
   }
 }
 
-module.exports = SpotifyClientWrapper
+module.exports = SpotifyClientWrapper;
